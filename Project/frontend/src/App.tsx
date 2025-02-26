6319d1b6d302c29b16ba62d67e35301e7040344b
xLightless <lightlessgaming@gmail.com>
2025-02-26 16:19:17 +0000

    Testing eslint.

--- Project/frontend/src/App.tsx
+++ Project/frontend/src/App.tsx
@@ -3,9 +3,11 @@ import './App.css'
 
 function App() {
 
+  const main = "Main";
+
   return (
     <div className=''>
-      <h1 className='text-responsive'>Car Insurance Web App.</h1>
+      <h1 className='text-responsive'>{main} Car Insurance Web App.</h1>
       <p className='text-responsive'>DO NOT use the App.tsx to creating indexes.</p>
       </div>
   )
