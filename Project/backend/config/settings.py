8300b041b912ea4bf3ddc79877362e0c981298b8
kanehill1999 <80599890+kanehill1999@users.noreply.github.com>
2025-03-05 12:21:12 +0000

    Update settings.py

--- Project/backend/config/settings.py
+++ Project/backend/config/settings.py
@@ -51,13 +51,7 @@ TWILIO_AUTH_TOKEN = os.getenv('TWILIO_AUTH_TOKEN')
 TWILIO_FROM_EMAIL = os.getenv('TWILIO_FROM_EMAIL')
 TWILIO_VERIFY_SERVICE_SID = os.getenv('TWILIO_VERIFY_SERVICE_SID') 
 
-# Email Backend
-#EMAIL_BACKEND = os.getenv('EMAIL_BACKEND', 'django.core.mail.backends.smtp.EmailBackend')
-#EMAIL_HOST = os.getenv('EMAIL_HOST', 'smtp.example.com')
-#EMAIL_PORT = int(os.getenv('EMAIL_PORT', 587))
-#EMAIL_USE_TLS = os.getenv('EMAIL_USE_TLS', 'True') == 'True'
-#EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-#EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
 
 
 MIDDLEWARE = [
