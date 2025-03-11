13a488a1ff7cf89eb15b5c5fbeface1f3c763809
xLightless <lightlessgaming@gmail.com>
2025-03-11 00:19:33 +0000

    Initial commit of early stages of the Admin Dashboard. Includes a reactive sidebar and searching for small to large displays.

--- Project/frontend/src/App.tsx
+++ Project/frontend/src/App.tsx
@@ -1,28 +1,18 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
-import Home from './templates/home/Home'
-import Nav from './components/navigation/Nav'
-import Footer from './components/footer/Footer'
-import Contact from './templates/contact/Contact'
+import { Routes, Route, BrowserRouter as Router } from 'react-router-dom'
+// import Home from './templates/home/Home'
+// import Nav from './components/navigation/Nav'
+// import Footer from './components/footer/Footer'
+// import Contact from './templates/contact/Contact'
+import AdminDashboard from './templates/admin'
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="flex flex-col min-h-screen">
-        <Nav />
-
-        <main className="flex-grow flex justify-center">
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="/home" element={<Home />} />
-            <Route path="/contact" element={<Contact />} />
-          </Routes>
-        </main>
-        
-        <Footer />
-      </div>
-    </BrowserRouter>
+    <Router>
+      <Routes>
+        <Route path="/admin-dashboard" element={<AdminDashboard />} />
+      </Routes>
+    </Router>
   )
 }
 
-export default App
+export default App;
