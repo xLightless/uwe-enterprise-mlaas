bf2664dbc96e9cdac847aefa8402958d2e7637ff
xLightless <lightlessgaming@gmail.com>
2025-03-12 03:29:56 +0000

    Added Network Activity Tab with Paginated Table data and analytics for API Calls.

--- Project/frontend/src/common/interfaces.ts
+++ Project/frontend/src/common/interfaces.ts
@@ -12,3 +12,43 @@ export interface SidebarItem {
     sideBarIcon: JSX.Element;
     items: [string, () => void | null, JSX.Element | null][];
 };
+
+export interface TableRow {
+    [key: string]: string | number | Date;
+}
+
+export interface TableData {
+    thead: string[];
+    tbody: TableRow[];
+    maxRowsPerPage?: number;
+}
+
+export interface ReactChildProp {
+    children: React.ReactNode;
+
+    /**
+     * When clicking on an id of a user this updates
+     * the interface with the user id's information.
+     * @param userId
+     */
+    onUserIdClick?: (userId: number) => void;
+};
+
+export interface UserProps {
+    user: {
+        userId?: number;
+        roleId?: number;
+        fullName?: string;
+        email?: string;
+        password?: string;
+        phoneNumber?: string;
+        created_at?: Date;
+        updated_at?: Date;
+        last_login?: Date;
+        isVerified?: boolean;
+        isActive?: boolean;
+        isAdmin?: boolean;
+        isStaff?: boolean;
+        isSuperuser?: boolean;
+    }
+}
--- Project/frontend/src/templates/admin/index.tsx
+++ Project/frontend/src/templates/admin/index.tsx
@@ -1,4 +1,4 @@
-import React, { JSX } from 'react';
+import React, { JSX, useEffect, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
     faCommentNodes,
@@ -11,42 +11,65 @@ import {
 } from '@fortawesome/free-solid-svg-icons';
 import { SidebarItem } from '../../common/interfaces';
 import Dashboard from '../../components/dashboard';
+import NetworkActivity from './content/system-activity/network-activity';
+import AuditLogs from './content/system-activity/audit-logs';
 
 const AdminDashboard: React.FC = () => {
+    const [displayChoice, setDisplayChoice] = useState<string | null>(null);
+
+    function onSidebarUpdate(name: string) {
+        setDisplayChoice(name);
+    };
+
+    const componentMapping: { [key: string]: React.FC } = {
+        "Network Activity": NetworkActivity,
+        "Audit Logs": AuditLogs,
+        // "MLAAS Traffic": MLAAS_Traffic,
+        // "Roles & Permissions": RolesPermissions,
+        // "Recovery": Recovery,
+        // "Data & Regulation": DataRegulation,
+        // "Invoices": Invoices,
+        // "Billing": Billing,
+    };
 
     const sidebarItems: SidebarItem[] = [
         {
             name: "System Activity",
             sideBarIcon: <FontAwesomeIcon icon={faServer} />,
             items: [
-                ["Network Activity", () => {}, <FontAwesomeIcon icon={faNetworkWired} />],
-                ["Audit Logs", () => {}, <FontAwesomeIcon icon={faCommentNodes} />],
-                ["MLAAS Traffic", () => {}, <FontAwesomeIcon icon={faMicrochip} />],
+                ["Network Activity", () => onSidebarUpdate("Network Activity"), <FontAwesomeIcon icon={faNetworkWired} />],
+                ["Audit Logs", () => onSidebarUpdate("Audit Logs"), <FontAwesomeIcon icon={faCommentNodes} />],
+                ["MLAAS Traffic", () => onSidebarUpdate("MLAAS Traffic"), <FontAwesomeIcon icon={faMicrochip} />],
             ] as [string, () => void | null, JSX.Element | null][],
         },
         {
-            name: "Manage Users",
+            name: "Manage Accounts",
             sideBarIcon: <FontAwesomeIcon icon={faUser} />,
             items: [
-                ["Roles & Permissions", () => {}, <FontAwesomeIcon icon={faNetworkWired} />],
-                ["Recovery", () => {}, <FontAwesomeIcon icon={faCommentNodes} />],
-                ["Data & Regulation", () => {}, <FontAwesomeIcon icon={faMicrochip} />],
+                ["Roles & Permissions", () => onSidebarUpdate("Roles & Permissions"), <FontAwesomeIcon icon={faNetworkWired} />],
+                ["Recovery", () => onSidebarUpdate("Recovery"), <FontAwesomeIcon icon={faCommentNodes} />],
+                ["Data & Regulation", () => onSidebarUpdate("Data & Regulation"), <FontAwesomeIcon icon={faMicrochip} />],
             ] as [string, () => void | null, JSX.Element | null][],
         },
         {
             name: "Review Finances",
             sideBarIcon: <FontAwesomeIcon icon={faCreditCard} />,
             items: [
-                ["Invoices", () => {}, <FontAwesomeIcon icon={faFileInvoice} />],
-                ["Billing", () => {}, <FontAwesomeIcon icon={faCreditCard} />],
+                ["Invoices", () => onSidebarUpdate("Invoices"), <FontAwesomeIcon icon={faFileInvoice} />],
+                ["Billing", () => onSidebarUpdate("Billing"), <FontAwesomeIcon icon={faCreditCard} />],
             ] as [string, () => void | null, JSX.Element | null][],
         },
     ];
 
+    /**
+     * Creates a map of components and render
+     * them based on the sidebar item clicked.
+     */
+    const DisplayComponent = displayChoice ? componentMapping[displayChoice] : null;
 
     return (
         <Dashboard sideBarItems={sidebarItems}>
-            <></>
+            {DisplayComponent ? <DisplayComponent /> : null}
         </Dashboard>
     )
 };
