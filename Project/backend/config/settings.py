a0ff1755006a1e8ba3c4e86960452b3c246abaf4
kanehill1999 <80599890+kanehill1999@users.noreply.github.com>
2025-03-24 20:28:21 +0000

    cors fix

--- Project/backend/config/settings.py
+++ Project/backend/config/settings.py
@@ -59,6 +59,7 @@ TWILIO_FROM_EMAIL = os.getenv('TWILIO_FROM_EMAIL')
 TWILIO_VERIFY_SERVICE_SID = os.getenv('TWILIO_VERIFY_SERVICE_SID')
 
 MIDDLEWARE = [
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -66,7 +67,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
-    'corsheaders.middleware.CorsMiddleware',
+
 ]
 
 ROOT_URLCONF = "config.urls"
