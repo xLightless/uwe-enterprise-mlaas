da88329b72ff8b8638a522a19021be9037fe725a
kanehill1999 <80599890+kanehill1999@users.noreply.github.com>
2025-03-28 06:08:24 +0000

    flake 8 fixes
    just fixing flake 8

--- Project/backend/function/Model_management/models.py
+++ Project/backend/function/Model_management/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 class Model(models.Model):
     model_id = models.AutoField(primary_key=True)
     model_name = models.CharField(max_length=255)
@@ -9,12 +10,12 @@ class Model(models.Model):
     is_active = models.BooleanField(default=False)
     num_accepted_claims = models.IntegerField(default=0)
     num_rejected_claims = models.IntegerField(default=0)
-    model_file = models.FileField(upload_to='models/', blank=True, null=True)  
-
+    model_file = models.FileField(upload_to='models/', blank=True, null=True) 
 
     class Meta:
         db_table = 'Models'
         app_label = "Model_management"
+
 
 class Prediction(models.Model):
     prediction_id = models.AutoField(primary_key=True)
@@ -23,11 +24,14 @@ class Prediction(models.Model):
     class Meta:
         db_table = 'Predictions'
 
+
 class UserModelFeedback(models.Model):
     feedback_id = models.AutoField(primary_key=True)
     user_id = models.IntegerField()
-    settlement_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
-    expected_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    settlement_amount = models.DecimalField(max_digits=10, decimal_places=2, 
+                                            default=0)
+    expected_amount = models.DecimalField(max_digits=10, decimal_places=2, 
+                                            default=0)
     feedback_rating = models.SmallIntegerField(default=0)
     comments = models.TextField(blank=True, null=True)
 
